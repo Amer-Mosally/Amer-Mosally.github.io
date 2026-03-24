52d2084074d380de8332ba245c157c9ca59c0cd1
Amer-Mosally <63898204+Amer-Mosally@users.noreply.github.com>
2026-03-25 01:05:56 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class', // <--- THIS IS THE MAGIC LINE
+  theme: { extend: {} },
   plugins: [],
 }
